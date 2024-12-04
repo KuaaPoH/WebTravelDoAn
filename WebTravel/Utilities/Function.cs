817c2c31567b4afeaf9fb7761ef5de45b9fecbab
Kenn Tran <trankenn04@gmail.com>
2024-12-04 14:49:55 +0700

    Upload BlogDetail :>>

---
+++ WebTravel/Utilities/Function.cs
@@ -0,0 +1,10 @@
+﻿namespace WebTravel.Utilities
+{
+    public class Function
+    {
+        public static string TitleSlugGenerationAlias(string title)
+        {
+            return SlugGenerator.SlugGenerator.GenerateSlug(title);
+        }
+    }
+}
